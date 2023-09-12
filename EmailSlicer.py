00e98e179a46f8bf44001a7db6938499664d798d
John (Chun Han) Lee <clee93@nait.ca>
2023-09-12 13:18:13 -0600

    implemented a try and except catch

--- EmailSlicer.py
+++ EmailSlicer.py
@@ -1,6 +1,10 @@
-email = input("Please enter a valid email: ").strip()
-
-username = email[:email.index('@')]
-domain = email[email.index('@') + 1:]
-
-print(f"Your username is {username} and your domain is {domain}")
+try:
+    email = input("Please enter a valid email: ").strip()
+    if '@' not in email:
+        raise Exception("That is not a valid email format")
+except:
+    print("That is not a valid email format")
+else:
+    username = email[:email.index('@')]
+    domain = email[email.index('@') + 1:]
+    print(f"Your username is {username} and your domain is {domain}")
