1bda78cdbef12b05646f0555e5fa2110638aec00
John (Chun Han) Lee <clee93@nait.ca>
2023-09-12 13:28:35 -0600

    refined exception error to catch invalid domain format

--- EmailSlicer.py
+++ EmailSlicer.py
@@ -2,8 +2,10 @@ try:
     email = input("Please enter a valid email: ").strip()
     if '@' not in email:
         raise Exception("That is not a valid email format")
-except:
-    print("That is not a valid email format")
+    if '.' not in email[email.index('@') + 1:]:
+        raise Exception("This is not a valid domain")
+except Exception as e:
+    print(e)
 else:
     username = email[:email.index('@')]
     domain = email[email.index('@') + 1:]
